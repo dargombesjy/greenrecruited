d0c4709b021897977f340ec00ed83900e3fac37b
dargombesjy <dargombesjy@gmail.com>
2026-03-17 12:04:20 +0700

    commit_170320206_01

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,76 +1,65 @@
 <script>
 	// import { Heading, P, Card, Button } from 'flowbite-svelte';
-	import { ArrowRightOutline } from 'flowbite-svelte-icons';
-	let vCard = false;
-	
+	// import { ArrowRightOutline } from 'flowbite-svelte-icons';
+	// let vCard = false;
+	import { PUBLIC_STRAPI_URL } from '$env/static/public';
+	import RightArrow from '$lib/RightArrow.svelte';
+	let { data } = $props();
 </script>
 
 <div class="mx-8 flex h-screen pb-8">
 	<div class="my-auto">
 		<h2 class="mb-4 text-[60pt] font-bold text-primary-50 dark:text-primary-500">GreenRecruitED</h2>
 		<div class="text-[28pt]">Sustainable Recruitment Learning Platform</div>
-		<div class="text-[28pt]">"Learning Recruitment for a Sustainable Future"</div>
+		<!-- <div class="text-[28pt]">"Learning Recruitment for a Sustainable Future"</div> -->
+		 <div class="text-[28pt]">{data.data.subtitle}</div>
 	</div>
 </div>
 
 <div class="mx-8 flex h-screen flex-col">
-	<video class="w-full" src="/videos/sample01.mp4" controls><track kind="captions" /></video>
+	<video class="w-full" muted autoplay loop playsinline controls disablepictureinpicture>
+		<!-- <track kind="captions" /> -->
+		<source src="{PUBLIC_STRAPI_URL}{data.data.intro_video.url}">
+	</video>
 </div>
 
 <div class="mt-10 grid h-screen grid-cols-3 content-center gap-2">
-	<div class="bg-sky-100/75 hover:bg-sky-500 h-96">
-		<div class="m-6">
-			<p class="h-32 mb-2 text-[32pt] font-bold tracking-tight text-gray-900 dark:text-white">
-				LESSON PLAN
-      </p>
-			<p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">
-				Penjelasan Lesson Plan
-			</p>
-			<button class="text-center font-medium inline-flex items-center justify-center text-white bg-primary-50 hover:bg-primary-800 focus-within:ring-primary-300 px-5 py-2.5 text-sm focus-within:ring-4 focus-within:outline-hidden rounded-lg w-40">
-				Lebih jauh <ArrowRightOutline class="ms-2 h-6 w-6 text-white" />
-			</button>
+	{#each data.data.block_1s as block1_item}
+		<div class="h-96 bg-sky-100/75 hover:bg-sky-500">
+			<div class="m-6">
+				<p class="mb-2 h-32 text-[32pt] font-bold tracking-tight text-gray-900 dark:text-white">
+					{block1_item.title}
+				</p>
+				<p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">
+					{block1_item.description}
+				</p>
+				<button
+					class="inline-flex w-40 items-center justify-center rounded-lg bg-primary-50 px-5 py-2.5 text-center text-sm font-medium text-white focus-within:ring-4 focus-within:ring-primary-300 focus-within:outline-hidden hover:bg-primary-800"
+				>
+					Lebih jauh <RightArrow class="text-white" />
+				</button>
+			</div>
 		</div>
-	</div>
-
-  <div class="bg-sky-100/75 hover:bg-sky-500 h-96">
-		<div class="m-6">
-			<p class="h-32 mb-2 text-[32pt] font-bold tracking-tight text-gray-900 dark:text-white">
-				WEBSITE GUIDELINE
-      </p>
-			<p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">
-				Panduan penggunaan website
-			</p>
-			<button class="text-center font-medium inline-flex items-center justify-center text-white bg-primary-50 hover:bg-primary-800 focus-within:ring-primary-300 px-5 py-2.5 text-sm focus-within:ring-4 focus-within:outline-hidden rounded-lg w-40">
-				Lebih jauh <ArrowRightOutline class="ms-2 h-6 w-6 text-white" />
-			</button>
-		</div>
-	</div>
-
-  <div class="bg-sky-100/75 hover:bg-sky-500 h-96">
-		<div class="m-6">
-			<p class="h-32 mb-2 text-[32pt] font-bold tracking-tight text-gray-900 dark:text-white">
-				DEVELOPER & CONTACT
-      </p>
-			<p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">
-				Hubungi Developer
-			</p>
-			<button class="text-center font-medium inline-flex items-center justify-center text-white bg-primary-50 hover:bg-primary-800 focus-within:ring-primary-300 px-5 py-2.5 text-sm focus-within:ring-4 focus-within:outline-hidden rounded-lg w-40">
-				Lebih jauh <ArrowRightOutline class="ms-2 h-6 w-6 text-white" />
-			</button>
-		</div>
-	</div>
-	
+	{/each}
 </div>
 
-<div class="mt-10 grid h-screen grid-cols-3 grid-rows-3 gap-2">
-	<div class="">Leftmost</div>
-	<div class="col-start-2 row-start-2">Center</div>
-	<div class="col-start-3 row-start-3">Rightmost</div>
+<div class="mt-10 grid h-screen grid-cols-3 grid-rows-3 gap-2 content-center text-[20pt]">
+	<div class="rounded-[20px] bg-primary-50 content-center text-center text-white">
+		<p>{data.data.block_3s[0].title}</p>
+	</div>
+	<div class="col-start-2 row-start-2 rounded-[20px] bg-primary-50 content-center text-center text-white">
+		<p>{data.data.block_3s[1].title}</p>
+	</div>
+	<div class="col-start-3 row-start-3 rounded-[20px] bg-primary-50 content-center text-center text-white">
+		<p>{data.data.block_3s[2].title}</p>
+	</div>
 </div>
 
-<div class="mx-auto mt-10 h-screen flex flex-col justify-center">
+<div class="mx-auto mt-10 flex h-screen flex-col justify-center">
 	<div class="text-center">
-		<p>Pelajari lebih lanjut tentang rekrutmen berbasis keberlanjutan</p>
-		<button class="mt-4 cursor-pointer rounded-[30px] bg-primary-50 py-4 px-8 text-white">Selengkapnya..</button>
+		<p>{data.data.lebih_lanjut}</p>
+		<button class="mt-4 cursor-pointer rounded-[30px] bg-primary-50 px-8 py-4 text-white"
+			>Selengkapnya..</button
+		>
 	</div>
 </div>
--- src/routes/modules/+page.svelte
+++ src/routes/modules/+page.svelte
@@ -12,10 +12,10 @@
         <div class="bg-white overflow-hidden cursor-pointer hover:shadow-lg transition-all duration-300 relative">
             <a href="/modules/{item.documentId}" class="block" aria-label="product-wrapper">
                 <div class="w-full aspect-[8/6] p-3">
-                    <img src="{PUBLIC_STRAPI_URL}{item.image.url}" alt="" class="w-full h-full object-contain">
+                    <img src="{PUBLIC_STRAPI_URL}{item.image.url}" alt="" class="w-full h-full object-fill">
                 </div>
                 <div class="p-6">
-                    <hr class="border border-gray-300 mb-6" />P}{item.image.url}" a
+                    <hr class="border border-gray-300 mb-6" />
                     <div>
                         <h4>{item.title}</h4>
                         <h4>{item.description}</h4>
